a3702355af3ba6d53c3dc538cad54c4308a452d7
tanzeyy <tan_ze@outlook.com>
2019-06-11 21:14:04 +0800

    Update context manager

---
+++ manage_context/mirror.py
@@ -0,0 +1,52 @@
+import contextlib
+
+
+@contextlib.contextmanager
+def looking_glass():
+    import sys
+    original_write = sys.stdout.write
+
+    def reverse_write(text):
+        original_write(text[::-1])
+    
+    sys.stdout.write = reverse_write
+    yield 'JABBERWOCKY'
+    sys.stdout.write = original_write
+
+
+class LookingGlass:
+    def __enter__(self):
+        import sys
+        self.original_write = sys.stdout.write
+        sys.stdout.write = self.reverse_write
+        return 'JABBERWOCKY'
+    
+    def reverse_write(self, text):
+        self.original_write(text[::-1])
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        import sys
+        sys.stdout.write = self.original_write
+        if exc_type is ZeroDivisionError:
+            print('Please DO NOT divide by zero!')
+            return True
+
+@contextlib.contextmanager
+def looking_glass2():
+    import sys
+    original_write = sys.stdout.write
+
+    def reverse_write(text):
+        original_write(text[::-1])
+    
+    sys.stdout.write = reverse_write
+
+    msg = ''
+    try:
+        yield 'JABBERWOCKY'
+    except ZeroDivisionError:
+        msg = 'Please DO NOT divide by zero!'
+    finally:
+        sys.stdout.write = original_write
+        if msg:
+            print(msg)
